0828497ae0e8c6e0b5d389d670afd741406ef152
TheAngryPopo <joelharder4@gmail.com>
2023-09-15 15:37:03 -0400

    added custom font

---
+++ src/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./home.css";
+
+function Home() {
+    return (<>
+        <div className="backgroundLines"> 
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+        </div>
+        <h1 className="name">Joel Harder</h1>
+    </>);
+}
+
+export default Home;
